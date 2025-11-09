6e3c0edaca44184c3676adc046abc59fd4b8eed6
Talita Vitória <thalitinha246@gmail.com>
2025-11-08 21:19:31 -0300

    resolvendo divergências de merge

--- src/main/java/com/br/femmcode/femmcode/repositories/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.br.femmcode.femmcode.repositories;
-
-import com.eventos.model.Evento;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventoRepository extends MongoRepository<Evento, String> {
-}
